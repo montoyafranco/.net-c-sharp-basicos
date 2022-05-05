050f36321c428f3a0cc6f0cf0adb1d17b3b9cd61
montoyafranco <francomontoya9810@gmail.com>
2022-05-04 23:33:27 -0300

    Comentado . viendo pasage de parametros y condicionales en c#

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -19,27 +19,35 @@ namespace Giraffe
             //Console.WriteLine("Numero de recien elegido es : " + num1);
             //Console.ReadLine();
 
-            int[] luckyNumbers = { 4, 5, 6, 5, 6, 4, 2, 3, 1 };
-            luckyNumbers[1] = 200;
-            Console.WriteLine(luckyNumbers[1]);
-            Console.ReadLine();
+            //int[] luckyNumbers = { 4, 5, 6, 5, 6, 4, 2, 3, 1 };
+            //luckyNumbers[1] = 200;
+            //Console.WriteLine(luckyNumbers[1]);
+            //Console.ReadLine();
 
-            Console.Write("Clase a elegir para su avatar: ");
-            string clase__guerrero = Console.ReadLine();
-            Console.WriteLine("Clase elegida: " + clase__guerrero );
+            //Console.Write("Clase a elegir para su avatar: ");
+            //string clase__guerrero = Console.ReadLine();
+            //Console.WriteLine("Clase elegida: " + clase__guerrero );
 
-            Console.Write("Que arma tendra: ");
-            string clase__arma = Console.ReadLine();
-            Console.WriteLine("Arma elegida: " + clase__arma);
+            //Console.Write("Que arma tendra: ");
+            //string clase__arma = Console.ReadLine();
+            //Console.WriteLine("Arma elegida: " + clase__arma);
 
-            Console.WriteLine("Aventurero eres un " + clase__guerrero + " .Sabia eleccion , junto con tu arma elegida  " + clase__arma + "tienes muchas probabilidades de completar tu travesia" );
+            //Console.WriteLine("Aventurero eres un " + clase__guerrero + " .Sabia eleccion , junto con tu arma elegida  " + clase__arma + "tienes muchas probabilidades de completar tu travesia" );
 
+            //Si quiero crear un array vacio tengo que decir si o si cuanto va a tener de espacio
 
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            Console.WriteLine(friends[0]);
 
+            Console.WriteLine("Aca invoco un numero y le pasa un parametro: " + Doble(5));
 
+        }
 
-
-
+        static int Doble(int num)
+        {
+            int resultado = num * 2;
+            return resultado;
 
         }
 
